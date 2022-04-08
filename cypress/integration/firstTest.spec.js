6c70713161f0d953600a456b5eb31779f726bc57
Anna Sviridchuk <anna1995.sw@gmail.com>
2022-04-08 12:47:49 +0300

    lector about contains

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,1 +1,24 @@
 /// <reference types="Cypress"/>
+
+it('Search by parent button tag', () => {
+    cy.viewport(1800, 700)
+    cy.visit('https://docs.cypress.io/api/commands/eq#Arguments')
+    cy.contains('Search')
+})
+
+it('Search by tag', () => {
+    cy.viewport(1800, 700)
+    cy.visit('https://docs.cypress.io/api/commands/eq#Arguments')
+    cy.contains('span', 'Search')
+})
+
+it('Ignore register', () => {
+    cy.viewport(1800, 700)
+    cy.visit('https://docs.cypress.io/api/commands/eq#Arguments')
+    cy.contains('SEARCH', {matchCase: false})
+})
+
+it.only('Search with get', () => {
+    cy.visit('http://www.facebook.com/')
+    cy.get('form').contains('Забыли пароль?')
+})
